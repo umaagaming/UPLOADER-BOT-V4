5a1f9cafd401409de58e6dbd8a579fef7e16a4da
umaagaming <umaagaming31@gmail.com>
2025-05-11 15:07:18 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -11,36 +11,36 @@ logging.basicConfig(
 
 class Config(object):
     
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", ))
-    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7505083443:AAH58D5mWbSeEFK80IMZ0akWYFkpaNbuWkU")
+    API_ID = int(os.environ.get("API_ID", "29850843"))
+    API_HASH = os.environ.get("API_HASH", "fb8165e31dafe562ab2441be81cb565d")
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
-    MAX_FILE_SIZE = 2194304000
-    TG_MAX_FILE_SIZE = 2194304000
+    MAX_FILE_SIZE = 51200000000000
+    TG_MAX_FILE_SIZE = 51200000000000
     SESSION_STR = ""
-    FREE_USER_MAX_FILE_SIZE = 2194304000
+    FREE_USER_MAX_FILE_SIZE = 51200000000000
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
     
     OUO_IO_API_KEY = ""
     MAX_MESSAGE_LENGTH = 4096
-    PROCESS_MAX_TIMEOUT = 3600
-    DEF_WATER_MARK_FILE = "@UploaderXNTBot"
+    PROCESS_MAX_TIMEOUT = 0
+    DEF_WATER_MARK_FILE = "@urluploaderukbot"
 
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
 
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://umaagaming31:<db_password>@cluster0.rxpnabz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002668048171"))
     LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-    SESSION_NAME = "UploaderXNTBot"
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+    OWNER_ID = int(os.environ.get("OWNER_ID", "6774514204"))
+    SESSION_NAME = "urluploaderukbot"
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002668048171")
 
     TG_MIN_FILE_SIZE = 2194304000
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "urluploaderukbot")
     ADL_BOT_RQ = {}
 
     # Set False off else True
